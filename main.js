202ef54839033042ef5ca7abbb7fe11b4d9c5583
johnlee1204 <35163184+johnlee1204@users.noreply.github.com>
2018-07-10 16:02:18 -0500

    Attacking and settling cities works.
    When settling cities, unit information still changes when it shouldn't.

--- main.js
+++ main.js
@@ -3,12 +3,12 @@
 document.getElementById('buttonS').onclick = beginGame;
 var canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
-canvas.oncontextmenu = function(e) {
+canvas.oncontextmenu = function (e) {
     e.preventDefault();
 };
 canvas.style.backgroundColor = "black";
 canvas.width = window.screen.availWidth;
-canvas.height = window.screen.availHeight+40;
+canvas.height = window.screen.availHeight + 40;
 canvas.style.display = 'block';
 var ctx = canvas.getContext('2d');
 var currentImage = 0;
@@ -43,9 +43,18 @@ var forestTile = document.createElement('img');
 document.body.appendChild(forestTile);
 forestTile.src = 'images/forestTile.png';
 forestTile.style.display = 'none';
-var tileSize = 32;
-
-var currentNumsMap;
+var warrior = document.createElement("img");
+document.body.appendChild(warrior);
+warrior.src = 'images/warrior.png';
+warrior.style.display = 'none';
+var settler = document.createElement("img");
+document.body.appendChild(settler);
+settler.src = 'images/settler.png';
+settler.style.display = 'none';
+var scroll = document.createElement("img");
+document.body.appendChild(scroll);
+scroll.src = 'images/scroll.jpg';
+scroll.style.display = 'none';
 
 document.body.appendChild(waterTile);
 var images = {
@@ -56,9 +65,12 @@ var images = {
     sand: sandTile,
     city: cityTile,
     deepWater: deepWaterTile,
-    forest:forestTile
+    forest: forestTile,
+    scroll: scroll
 };
-var imagesAsNum = [blackTile, grassTile, rockTile, waterTile, sandTile, cityTile, deepWaterTile,forestTile];
+var imagesAsNum = [blackTile, grassTile, rockTile, waterTile, sandTile, cityTile, deepWaterTile, forestTile];
+var unitImagesAsNum = [warrior, settler];
+
 var xOffSet = 0;
 var yOffSet = 0;
 var up = true;
@@ -70,62 +82,321 @@ var mouseX;
 var mouseY;
 var currentFile;
 let drawing = false;
+var tileSize = 32;
 let board;
+let player1;
+var units = [];
+let unitSelected;
+let possibleMoves = [];
+let started = false;
+let moving = false;
+let updatingMovement = false;
+let warning = false;
+var cities = [];
 
+const playerId = prompt("Which Player are you?");
 
+function reset(){
+	var scope = this;
+    var oReq = new XMLHttpRequest(); //New request object
+    oReq.onload = function () {
+    	location.reload();
+    };
+    oReq.open("get", "php/GameRunner.php?resetgame=true", true);
+    oReq.send();
+}
 
 
 function beginGame() {
     var mapToLoadAsString;
     var scope = this;
     var oReq = new XMLHttpRequest(); //New request object
-    oReq.onload = function() {
+    oReq.onload = function () {
         //mapToLoadAsString = oReq.response;
         //scope.mapToLoad = mapToLoadAsString.split(',');
+        units = [];
         let serverReply = JSON.parse(oReq.response);
-       	board = serverReply.gameboard;
-       	//let player1 = serverReply.player1.unserialize();
-       	drawing = true;
+        board = serverReply.gameBoard;
+        player1 = serverReply.player1;
+        player2 = serverReply.player2;
+        units = serverReply.units;
+        /*for(let i = 0;i<player1.units.length;i++){
+            units.push(player1.units[i]);
+        }*/
+        drawing = true;
+        update();
+        updatingMovement = false;
     };
-    oReq.open("get", "php/GameRunner.php", true);
+    oReq.open("get", "php/GameRunner.php?start=true&nextturn=true", true);
     oReq.send();
     tileSize = 32;
     document.getElementById('startButton').style.display = "none";
     document.getElementById('buttonS').style.display = "none";
 }
 
-function drawBoard(board){
+function drawBoard() {
     for (let i = 0; i < board.tiles.length; i++) {
         for (let j = 0; j < board.tiles[i].length; j++) {
-        	if(imagesAsNum[board.tiles[i][j].image]){
-       		     ctx.drawImage(imagesAsNum[board.tiles[i][j].image],j*tileSize-xOffSet,i*tileSize-yOffSet,tileSize,tileSize);
-       		}
+            if (imagesAsNum[board.tiles[i][j].image]) {
+                if (possibleMoves.includes(board.tiles[i][j])) {
+                	ctx.globalAlpha = .5;
+                    ctx.drawImage(imagesAsNum[0], j * tileSize - xOffSet, i * tileSize - yOffSet, tileSize, tileSize);
+                    ctx.globalAlpha = 1;
+                }
+                else {
+                    ctx.drawImage(imagesAsNum[board.tiles[i][j].image], j * tileSize - xOffSet, i * tileSize - yOffSet, tileSize, tileSize);
+                }
+            }
+        }
+    }
+    for (let i = 0; i < units.length; i++) {
+        ctx.drawImage(unitImagesAsNum[units[i].image], units[i].posTile.col * tileSize - xOffSet + .25 * tileSize, units[i].posTile.row * tileSize - yOffSet + .25 * tileSize, tileSize / 2, tileSize / 1.5);
+    }
+
+    if(unitSelected){
+    	ctx.fillStyle = "black";
+    	ctx.drawImage(images.scroll,0,canvas.height*.7,canvas.width*.2,canvas.height*.3);
+    	ctx.fillStyle = "black";
+    	ctx.font = "20px Georgia"
+    	ctx.fillText("Unit: "+unitSelected.name.replace('\\',''),40,canvas.height*.7+50);
+    	ctx.fillText("Health: "+unitSelected.health,40,canvas.height*.7+120);
+    	ctx.fillText("Attack: "+unitSelected.attack,40,canvas.height*.7+190);
+    	ctx.fillText("Defense: "+unitSelected.defense,40,canvas.height*.7+260);
+    }
+
+    if (unitSelected && possibleMoves.length == 0 && unitSelected.playerId == playerId) {
+        let mp = unitSelected.movementPoints;
+        let pos = unitSelected.posTile;
+
+        for (let i = 1; i <= mp; i++) {
+        	if(parseInt(pos.row) - i>=0){
+            	possibleMoves.push(board.tiles[parseInt(pos.row) - i][parseInt(pos.col)].image != "3" && board.tiles[parseInt(pos.row) - i][parseInt(pos.col)].image != "6" ? board.tiles[parseInt(pos.row) - i][parseInt(pos.col)] : null);
+        	}
+        	if(parseInt(pos.row) + i < board.tiles.length){
+            	possibleMoves.push(board.tiles[parseInt(pos.row) + i][parseInt(pos.col)].image != "3" && board.tiles[parseInt(pos.row) + i][parseInt(pos.col)].image != "6" ? board.tiles[parseInt(pos.row) + i][parseInt(pos.col)] : null);
+            }
+            if(parseInt(pos.col) - i>=0){
+            	possibleMoves.push(board.tiles[parseInt(pos.row)][parseInt(pos.col) - i].image != "3" && board.tiles[parseInt(pos.row)][parseInt(pos.col) - i].image != "6" ? board.tiles[parseInt(pos.row)][parseInt(pos.col) - i] : null);
+            }
+            if(parseInt(pos.col) + i<board.tiles[0].length){
+            	possibleMoves.push(board.tiles[parseInt(pos.row)][parseInt(pos.col) + i].image != "3" && board.tiles[parseInt(pos.row)][parseInt(pos.col) + i].image != "6" ? board.tiles[parseInt(pos.row)][parseInt(pos.col) + i] : null);
+            }
+            if (i !== 0 && i % 2 == 0) {
+            	if(parseInt(pos.row) - i / 2>=0&&parseInt(pos.col) - i / 2>=0){
+                	possibleMoves.push(board.tiles[parseInt(pos.row) - i / 2][parseInt(pos.col) - i / 2].image != "3" && board.tiles[parseInt(pos.row) - i / 2][parseInt(pos.col) - i / 2].image != "6" ? board.tiles[parseInt(pos.row) - i / 2][parseInt(pos.col) - i / 2] : null);
+                }
+                if(parseInt(pos.row) + i / 2<board.tiles.length&&parseInt(pos.col) + i / 2<board.tiles[0].length){
+                	possibleMoves.push(board.tiles[parseInt(pos.row) + i / 2][parseInt(pos.col) + i / 2].image != "3" && board.tiles[parseInt(pos.row) + i / 2][parseInt(pos.col) + i / 2].image != "6" ? board.tiles[parseInt(pos.row) + i / 2][parseInt(pos.col) + i / 2] : null);
+                }
+                if(parseInt(pos.row) - i / 2>=0&&parseInt(pos.col) + i / 2<board.tiles[0].length){
+                	possibleMoves.push(board.tiles[parseInt(pos.row) - i / 2][parseInt(pos.col) + i / 2].image != "3" && board.tiles[parseInt(pos.row) - i / 2][parseInt(pos.col) + i / 2].image != "6" ? board.tiles[parseInt(pos.row) - i / 2][parseInt(pos.col) + i / 2] : null);
+                }
+                if(parseInt(pos.row) + i / 2<board.tiles.length&&parseInt(pos.col) - i / 2>=0){
+                	possibleMoves.push(board.tiles[parseInt(pos.row) + i / 2][parseInt(pos.col) - i / 2].image != "3" && board.tiles[parseInt(pos.row) + i / 2][parseInt(pos.col) - i / 2].image != "6" ? board.tiles[parseInt(pos.row) + i / 2][parseInt(pos.col) - i / 2] : null);
+                }
+            }
+        }
+        update();
+    }
+    else if (!unitSelected) {
+        possibleMoves = [];
+	}
+	if(warning){
+	        ctx.fillStyle = "black";
+	    	ctx.drawImage(images.scroll,canvas.width/5*2,0,canvas.width/5,canvas.height*.1);
+	    	ctx.fillStyle = "black";
+	    	ctx.font = "20px Georgia"
+	    	ctx.fillText("You can't control that player",canvas.width/5*2+10,20);
+		}
+}
+
+
+function endTurn() {
+    if (drawing) {
+        var scope = this;
+        var oReq = new XMLHttpRequest(); //New request object
+        oReq.onload = function () {
+            if (oReq.response && isJson(oReq.response)) {
+                let serverReply = JSON.parse(oReq.response);
+                if (serverReply.gameBoard && serverReply.player1) {
+                    units = [];
+                    board = serverReply.gameBoard;
+                    player1 = serverReply.player1;
+                    player2 = serverReply.player2;
+                    units = serverReply.units;
+                    drawing = true;
+                    update();
+                }
+            }
+        };
+        oReq.open("get", "php/GameRunner.php?nextturn=true&update=true", true);
+        oReq.send();
+    }
+}
+
+function changeView() {
+    if (drawing) {
+        let tempX = xOffSet;
+        let tempY = yOffSet;
+        if (up && board.tiles[0][0].y + yOffSet > -9) {
+            yOffSet -= 20;
+        }
+        if (down && board.tiles.length * tileSize - yOffSet > canvas.height - 9) {
+            yOffSet += 20;
+        }
+        if (right && board.tiles[0].length * tileSize - xOffSet > canvas.width - 9) {
+            xOffSet += 20;
+        }
+        if (left && board.tiles[0][0].x + xOffSet > 9) {
+
+            xOffSet -= 20;
+        }
+        if (tempX != xOffSet || tempY != yOffSet) {
+            update();
         }
     }
 }
 
-
-function endTurn(){
-
+function isJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 
-function changeView() {
-    if (up&&board.tiles[0][0].y+yOffSet>-9)
-    {
-    	yOffSet -= 10;
-    }
-    if (down&&board.tiles[board.tiles.length-1][0].y-yOffSet>canvas.height-tileSize-9)
-    { 
-    	yOffSet += 10;
-    }
-    if (right&&board.tiles[0][board.tiles[0].length-1].x-xOffSet>canvas.width-tileSize-9)
-    { 
-    	xOffSet += 10;
-    }
-    if (left&&board.tiles[0][0].x+xOffSet>9)
-    { 
 
-    	xOffSet -= 10;
+
+function moveUnit(unit, newX, newY) {
+    let newRow = Math.floor(newY / tileSize);
+    let newCol = Math.floor(newX / tileSize);
+    if(!moving&&board.tiles[newRow][newCol].image!="3"&&board.tiles[newRow][newCol].image!="6" && unit.playerId == playerId) {
+        moving = true;
+        let newRow = Math.floor(newY / tileSize);
+        let newCol = Math.floor(newX / tileSize);
+        let unitId = unit.id;
+        update();
+        //check for attack
+        let attacking = false;
+        let defendingId = null;
+        for(let i = 0; i< units.length;i++){
+        	if(units[i].posTile.row == newRow && units[i].posTile.col == newCol){
+        		attacking = true;
+        		defendingId = units[i].id;
+        	}
+        }
+        //attacking
+        if(attacking&&unitId!=defendingId){
+        	var scope = this;
+	        var oReq = new XMLHttpRequest(); //New request object
+	        oReq.onload = function () {
+	            if (oReq.response && isJson(oReq.response)) {
+                    let serverReply = JSON.parse(oReq.response);
+                    unitsFromServer = serverReply;
+                    units = unitsFromServer;
+                    update();
+                }
+	            moving = false;
+	        };
+	        oReq.open("get", "php/GameRunner.php?attack=true&attackUnitId=" + unitId + "&defendUnitId=" + defendingId+"&playerid="+playerId, true);
+	        oReq.send();
+        }
+        //not attacking
+        else{
+	        var scope = this;
+	        var oReq = new XMLHttpRequest(); //New request object
+	        oReq.onload = function () {
+	            if (oReq.response && isJson(oReq.response)) {
+	                let serverReply = JSON.parse(oReq.response);
+	                newTile = serverReply;
+	                unit.posTile.row = newTile.row;
+	                unit.posTile.col = newTile.col;
+	                update();
+	            }
+	            moving = false;
+	        };
+	        oReq.open("get", "php/GameRunner.php?move=true&unitId=" + unitId + "&newRow=" + newRow + "&newCol=" + newCol + "&update=true&playerid="+playerId, true);
+	        oReq.send();
+    	}
+    }
+    else if(moving){
+        alert("Please wait for the last movement to finish");
+    }
+    else if(unit.playerId != playerId)
+    {
+    	var count = 0;
+    	var warningInterval = setInterval(function(){
+    		warning = true;
+    		count++;
+	    	if(count>200){
+	    		warning = false;
+	    		clearInterval(warningInterval);
+	    	}
+    	},10);
+    	update();
+	}
+}
+
+function foundCity(unit){
+	let name = prompt("City Name?");
+	var scope = this;
+            var oReq = new XMLHttpRequest(); //New request object
+            oReq.onload = function () {
+            	console.log('1');
+                if (oReq.response && isJson(oReq.response)) {
+                    let serverReply = JSON.parse(oReq.response);
+                    console.log('2');
+	                if (serverReply.gameBoard) {
+	                    units = [];
+	                    board = serverReply.gameBoard;
+	                    units = serverReply.units;
+	                    cities = serverReply.cities;
+	                    console.log('all');
+	                    console.log(units);
+	                    drawing = true;
+	                    update();
+	                }
+                }
+            };
+            oReq.open("get", "php/GameRunner.php?nextturn=true&foundcity=true&playerid="+unit.playerId+"&name="+name+"&settlerid="+unit.id, true);
+            oReq.send();
+}
+
+
+function updateMovement() {
+    if(!updatingMovement&&!moving) {
+        updatingMovement = true;
+        if (drawing) {
+            var scope = this;
+            var oReq = new XMLHttpRequest(); //New request object
+            oReq.onload = function () {
+                if (oReq.response && isJson(oReq.response)&&!moving) {
+                    let serverReply = JSON.parse(oReq.response);
+                    unitsFromServer = serverReply;
+                    for (let i = 0; i < unitsFromServer.length; i++) {
+                        if (units[i].id == unitsFromServer[i].id) {
+                            units[i].posTile.row = unitsFromServer[i].posTile.row;
+                            units[i].posTile.col = unitsFromServer[i].posTile.col;
+                        }
+                    }
+                    update();
+                }
+                updatingMovement = false;
+            };
+            oReq.open("get", "php/GameRunner.php?updatemovement=true", true);
+            oReq.send();
+        }
+    }
+}
+
+function checkIfSelectingUnit(x, y) {
+    for (let i = 0; i < units.length; i++) {
+        if (Math.floor(x / tileSize) == units[i].posTile.col && Math.floor(y / tileSize) == units[i].posTile.row) {
+            unitSelected = units[i]
+            update();
+            return;
+        }
     }
 }
 
@@ -133,15 +404,14 @@ function update() {
     ctx.beginPath();
     ctx.fillStyle = 'black';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx.fillRect(0, 0, canvas.width, canvas.height);
-    if(drawing){
-    	changeView();
-    	drawBoard(board);
+    if (drawing) {
+        drawBoard();
     }
     ctx.fill();
     ctx.closePath();
 }
-document.addEventListener("keydown", function(event) {
+
+document.addEventListener("keydown", function (event) {
     switch (event.keyCode) {
         case 48: //zero
             currentImage = 0;
@@ -185,15 +455,19 @@ document.addEventListener("keydown", function(event) {
             alert("Sand");
             break;
         case 76: //l
+            updateMovement();
+            break;
+        case 78://n
+            endTurn();
             break;
         case 82: //r
+        reset();
             break;
         case 84: //t
             break;
         case 85: //u
             break;
         case 86: //v
-            beginGame();
             break;
         case 87: //w
             break;
@@ -202,6 +476,11 @@ document.addEventListener("keydown", function(event) {
             yOffSet = 0;
             break;
         case 83: //s key
+        	if(unitSelected&&unitSelected.image == 1){
+        		foundCity(unitSelected);
+        		unitSelected = false;
+        	}
+
             break;
         case 37: //left
             right = false;
@@ -216,13 +495,15 @@ document.addEventListener("keydown", function(event) {
             up = false;
             break;
         case 90: //z
-            if (tileSize*board.tiles[0].length >= window.screen.availWidth) {
+            if (drawing && tileSize * board.tiles[0].length >= window.screen.availWidth) {
                 tileSize /= 1.1;
+                update();
             }
             break;
         case 88: //x
-            if (tileSize < 128) {
+            if (drawing && tileSize < 128) {
                 tileSize *= 1.1;
+                update();
             }
             break;
 
@@ -231,21 +512,22 @@ document.addEventListener("keydown", function(event) {
             break;
     }
 });
-canvas.addEventListener("mousewheel", function(event) {
-    //alert(board.tiles[0][0].x+','+board.tiles[0][0].y);
-    if (event.deltaY > 0) {
-        if (tileSize*board.tiles[0].length >= window.screen.availWidth) {
+canvas.addEventListener("mousewheel", function (event) {
+    if (drawing && event.deltaY > 0) {
+        if (tileSize * board.tiles[0].length >= window.screen.availWidth) {
             tileSize /= 1.1;
+            update();
         }
     } else {
-        if (tileSize < 128) {
+        if (drawing && tileSize < 128) {
             tileSize *= 1.1;
+            update();
         }
     }
 });
 
 
-document.addEventListener("keyup", function(event) {
+document.addEventListener("keyup", function (event) {
     switch (event.keyCode) {
         case 37: //left
             right = true;
@@ -265,15 +547,23 @@ document.addEventListener("keyup", function(event) {
             break;
     }
 });
-canvas.addEventListener("mousedown", function(event) {
-    press = true;
+canvas.addEventListener("mousedown", function (event) {
+    if (drawing && !unitSelected) {
+        checkIfSelectingUnit(event.clientX + xOffSet, event.clientY + yOffSet);
+    }
+    else if (drawing && unitSelected) {
+        moveUnit(unitSelected, event.clientX + xOffSet, event.clientY + yOffSet);
+        unitSelected = false;
+    }
+    update();
 });
-canvas.addEventListener("mouseup", function(event) {
+canvas.addEventListener("mouseup", function (event) {
     press = false;
 });
-canvas.addEventListener("mousemove", function(event) {
+canvas.addEventListener("mousemove", function (event) {
     mouseX = event.clientX;
     mouseY = event.clientY;
 });
 
-setInterval(update, 10);
+setInterval(changeView, 10);
+setInterval(updateMovement, 500);
